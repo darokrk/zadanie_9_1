0e806fdf982eb09c92e437b9fb886ff1e13cf8e2
Dariusz Rosiek <daro.krk@interia.pl>
2017-12-27 22:54:30 +0100

    Change content in js/script.js, fixed problem with function

--- js/script.js
+++ js/script.js
@@ -1,19 +1,30 @@
-
+/*
 var a = prompt('Podaj wartosc A');
 var h = prompt('Podaj wartosc h');
-
+*/
 //start function
 
-	function getTriangleArea(a, h) {
-		if (a > 0 && h > 0) {
-			console.log('Prawidlowe dane');
-			alert('Prawidlowe dane');
-		}
-		else {
-			console.log('Nieprawidlowe dane');
-			alert('Nieprawidlowe dane');
-		}
+function getTriangleArea(a, h) {
+	if (a > 0 && h > 0) {
 		return a*h/2;
 	}
-	console.log(getTriangleArea(a, h));
-	
+	else {
+		console.log('Nieprawidlowe dane');
+		alert('Nieprawidlowe dane');
+	}	
+}
+
+var triangle1Area = getTriangleArea(-10, 15);
+var triangle2Area = getTriangleArea(5, 10);
+var triangle3Area = getTriangleArea(20, 8);
+
+
+
+console.log(triangle1Area);
+alert(triangle1Area);
+
+console.log(triangle2Area);
+alert(triangle2Area);
+
+console.log(triangle3Area);
+alert(triangle3Area);
